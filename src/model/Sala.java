c36a5193be6768f532b86461f3ebcd9715177f9a
Nanson Augusto <nanson.augusto@hotmail.com>
2013-11-30 19:13:43 -0200

    Melhorando as anotações.

--- src/model/Sala.java
+++ src/model/Sala.java
@@ -12,16 +12,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author augusto
  */
+
+@Table(name="t_sala")
 @Entity
 public class Sala implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_sala")
     private Integer id;
     
     @Column(name="razao_social")
