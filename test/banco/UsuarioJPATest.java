175c798965322d310f59fe9d51d1f4b273f8610d
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-01 18:56:45 -0200

    Modificando o teste criar.

--- test/banco/UsuarioJPATest.java
+++ test/banco/UsuarioJPATest.java
@@ -3,15 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package banco;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import jpa.UsuarioJpaController;
 import model.Usuario;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import seguranca.Criptografia;
 
 /**
  *
@@ -19,39 +23,38 @@ import static org.junit.Assert.*;
  */
 public class UsuarioJPATest {
     
-    public static void criarCampoTest(){
+    @Test
+    public void criarCampoTest() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("PokerBASEPU");
-        
-        Usuario usuario= new Usuario();
-        
-        usuario.setNome("Augusto");
-        usuario.setUsername("GalapagosBr");
-        usuario.setSenha("criptografia");
-        
         UsuarioJpaController usuarioJPA = new UsuarioJpaController(emf);
-        try{
+        Usuario usuario = new Usuario();
+
+        if (usuarioJPA.findUsuario("GalapagosBr") == null) {
+            usuario.setNome("Augusto");
+            usuario.setUsername("GalapagosBr");
+            
+            try {
+                usuario.setSenha(Criptografia.cifrar("fenix"));
+                System.out.println(usuario.getSenha());
+            } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                Logger.getLogger(UsuarioJPATest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
             usuarioJPA.create(usuario);
-        }catch(Exception e){
-            System.out.println("**************** Usuario ja cadastrado! *********************");
-            e.printStackTrace(System.out);
+            assertTrue(usuario.getId() != 0);
         }
-        
-        
-       // assertTrue(usuario.getId() != 0);
+
         emf.close();
     }
-    
+
     @Test
-    public void findUsuarioTest(){
-        criarCampoTest();
-        
+    public void findUsuarioTest() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("PokerBASEPU");
         UsuarioJpaController usuarioJPA = new UsuarioJpaController(emf);
-        
+
         Usuario findUsuario = usuarioJPA.findUsuario("GalapagosBr");
-        
         assertNotNull(findUsuario);
-        
+
         System.out.println(findUsuario.getId());
         emf.close();
     }
