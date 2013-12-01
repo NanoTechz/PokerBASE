d0c3ef225a6424ffab07e9c92c22455ca57dce62
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-01 16:23:40 -0200

    Add a anotação para o campo username ser único

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -31,7 +31,7 @@ public class Usuario implements Serializable {
 
     @Column(length = 50)
     private String nome;
-    @Column(length = 20)
+    @Column(length = 20, unique = true)
     private String username;
     @Column(length = 14)
     private String senha;
