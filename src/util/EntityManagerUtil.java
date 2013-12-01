d4bfff1b4efe7e73ca0b238de2f4d966f5bf5ec8
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-01 18:58:11 -0200

    Criando classe para auxiliar na hora de fazer a conexao com o banco.

---
+++ src/util/EntityManagerUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.hibernate.ejb.EntityManagerFactoryImpl;
+
+/**
+ *
+ * @author augusto
+ */
+public class EntityManagerUtil {
+    public static EntityManagerFactory emf;
+    
+    static{
+        EntityManagerUtil.emf = Persistence.createEntityManagerFactory("PokerBASEPU");
+    }
+}
