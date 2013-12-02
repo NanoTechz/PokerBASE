9a7f34b2105d3d8f6f4644fdb7f57946365f5a20
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-01 22:06:50 -0200

    Adicionado o controller do cadastro de usuario.

---
+++ src/controller/CadastroController.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManagerFactory;
+import jpa.UsuarioJpaController;
+import model.Usuario;
+import seguranca.Criptografia;
+import view.CadastroUsuarioDialog;
+
+/**
+ *
+ * @author augusto
+ */
+public class CadastroController {
+
+    private CadastroUsuarioDialog cadastroView;
+    private Usuario usuario;
+    private EntityManagerFactory emf;
+    private UsuarioJpaController usuarioJPA;
+
+    public CadastroController(CadastroUsuarioDialog cadastroView, Usuario usuario, EntityManagerFactory emf) {
+        this.cadastroView = cadastroView;
+        this.usuario = usuario;
+        this.emf = emf;
+        this.usuarioJPA = new UsuarioJpaController(emf);
+
+        this.cadastroView.addAdicionarListener(new AdicionarListener());
+    }
+
+    class AdicionarListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if (cadastroView.getSenha().isEmpty() || cadastroView.getSenhaConfirmaacao().isEmpty()
+                    || cadastroView.getNome().isEmpty() || cadastroView.getUserName().isEmpty()) {
+                cadastroView.mensagemErro("Algum campo vazio!");
+                return;
+            }
+
+            if (!(verificarSenha(cadastroView.getSenha(), cadastroView.getSenhaConfirmaacao()))) {
+                cadastroView.mensagemErro("Senhas não batem!");
+                cadastroView.limparCampoSenha();
+                return;
+            }
+
+            if (verificarUserName(cadastroView.getUserName())) {
+                cadastroView.mensagemErro("Username já em uso");
+                return;
+            }
+            
+            usuario = new Usuario();
+            
+            usuario.setNome(cadastroView.getNome());
+            usuario.setUsername(cadastroView.getUserName());
+            
+            try {
+                usuario.setSenha(Criptografia.cifrar(cadastroView.getSenha()));
+                usuarioJPA.create(usuario);
+                
+                if(usuario.getId() != 0){
+                     cadastroView.mensagemErro("Usuario cadastrado com sucesso!");
+                }
+            } catch (    NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+                cadastroView.mensagemErro("Erro inesperado!");
+                Logger.getLogger(CadastroController.class.getName()).log(Level.SEVERE, null, ex); 
+            }finally{
+                cadastroView.dispose();
+            }
+        }
+    }
+
+    private boolean verificarSenha(String senha, String confirmacao) {
+        return senha.equals(confirmacao);
+    }
+
+    private boolean verificarUserName(String userName) {
+        return (usuarioJPA.findUsuario(userName) != null);
+    }
+
+}
