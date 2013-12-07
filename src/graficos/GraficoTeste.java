6d1a0667a916727cd34acbf83afbab51dd46d458
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-07 19:18:16 -0200

    Gráfico básico.

---
+++ src/graficos/GraficoTeste.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package graficos;
+
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.editor.ChartEditorFactory;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.time.RegularTimePeriod;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+/**
+ *
+ * @author augusto
+ */
+public class GraficoTeste {
+    private JFreeChart grafico;
+    private XYSeriesCollection datasets;
+
+    public GraficoTeste(String title, String labelX, String labelY) {
+        datasets = new XYSeriesCollection();
+        
+        grafico = ChartFactory.createXYLineChart(title, labelY, labelY, datasets);
+          
+    }
+    
+    public void addSerie(XYSeries serie){
+        datasets.addSeries(serie);
+    }
+
+
+    public JPanel getPanel(){
+        return new ChartPanel(grafico);
+    }
+
+    public JFreeChart getGrafico() {
+        return grafico;
+    }
+}
