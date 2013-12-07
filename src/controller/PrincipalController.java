0b438db1985044b1178f124a9aeed32a1429ca46
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-07 19:18:49 -0200

    Add. método para controlar o gráfico.

--- src/controller/PrincipalController.java
+++ src/controller/PrincipalController.java
@@ -3,19 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controller;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import jpa.BankrollJpaController;
+import jpa.CashJpaController;
 import jpa.SalaJpaController;
 import jpa.SessaoJpaController;
+import jpa.TorneioJpaController;
 import jpa.UsuarioJpaController;
 import model.Bankroll;
+import model.Cash;
 import model.Sessao;
+import model.Torneio;
 import model.Usuario;
+import org.jfree.data.xy.XYSeries;
 import util.FecharConexaoWindowListener;
 import view.CadastroBankrollDialog;
 import view.PrincipalFrame;
@@ -24,10 +28,11 @@ import view.PrincipalFrame;
  *
  * @author augusto
  */
-public class PrincipalController extends Controller{
+public class PrincipalController extends Controller {
+
     private Usuario usuario;
     private PrincipalFrame principalView;
-    
+
     private UsuarioJpaController usuarioJPA;
     private BankrollJpaController bankrollJPA;
     private SessaoJpaController sessaoJPA;
@@ -37,32 +42,74 @@ public class PrincipalController extends Controller{
         super(emf);
         this.usuario = usuario;
         this.principalView = principalView;
-        
+
         this.principalView.setUserName(usuario.getUsername());
         this.principalView.centralizarTela();
         this.principalView.addWindowListener(new FecharConexaoWindowListener());
-        
+
         this.usuarioJPA = new UsuarioJpaController(emf);
         this.bankrollJPA = new BankrollJpaController(emf);
         this.salaJPA = new SalaJpaController(emf);
         this.sessaoJPA = new SessaoJpaController(emf);
-        
+
         pegarInfoUsuario(usuario);
-        
-        if(usuario.getListaBankRolls().isEmpty()){
+
+        if (usuario.getListaBankRolls().isEmpty()) {
             CadastroBankrollDialog cadastroView = new CadastroBankrollDialog(principalView, true);
             CadastroBankRollController bankrollController = new CadastroBankRollController(cadastroView, usuario, emf);
-            
+
             cadastroView.setVisible(true);
         }
-        
+
+        gerarGrafico();
     }
-    
-    public void pegarInfoUsuario(Usuario usuario){
+
+    public void pegarInfoUsuario(Usuario usuario) {
         List<Bankroll> listaBankRoll = bankrollJPA.findBankRollUsuario(usuario);
         usuario.setListaBankRolls(listaBankRoll);
-        
+
         List<Sessao> listaSessao = sessaoJPA.findSessaoUsuario(usuario);
         usuario.setListaSessao(listaSessao);
+    }
+
+    private void gerarGrafico() {
+        TorneioJpaController torneioJpaController = new TorneioJpaController(getEmf());
+        List<Torneio> findTorneioUsuario = torneioJpaController.findTorneioUsuario(usuario);
+        
+        CashJpaController cashJpaController = new CashJpaController(getEmf());
+        List<Cash> findCashUsuario = cashJpaController.findCashUsuario(usuario);
+        
+        XYSeries serie = new XYSeries("torneios");
+        XYSeries cashSeries = new XYSeries("cash");
+        XYSeries serieRoiEsperado = new XYSeries("roi");
+        
+        double soma = 0, somaAjuste = 0, roi, mediaBuyIn, totalBuyIN;
+        
+       totalBuyIN = torneioJpaController.totalBuyIn(usuario); 
+       roi = (torneioJpaController.totalValorGanho(usuario) - totalBuyIN)/totalBuyIN;
+       mediaBuyIn = totalBuyIN/findTorneioUsuario.size();
+        
+        for (int i = 0; i < findTorneioUsuario.size(); i++) {
+            soma+= findTorneioUsuario.get(i).getValorGanho() - findTorneioUsuario.get(i).getBuyIn();
+            
+            serie.add(i+1, soma);
+            
+            somaAjuste+= mediaBuyIn*roi;
+            serieRoiEsperado.add(i,somaAjuste);
+            
+        }
+        
+        int count=1;
+        soma = 0;
+        for (Cash cash : findCashUsuario) {
+            soma += cash.getValorGanho();
+            cashSeries.add(count, soma, true);
+            
+            count++;
+        }
+        
+        this.principalView.addSerie(serie);
+        this.principalView.addSerie(serieRoiEsperado);
+        this.principalView.addSerie(cashSeries);
     }
 }
