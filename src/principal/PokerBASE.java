1ca075e277f2e4ff9431cfe0ff87151c85ceb7e3
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-07 19:17:08 -0200

    Add. Look and Feel.

--- src/principal/PokerBASE.java
+++ src/principal/PokerBASE.java
@@ -6,6 +6,13 @@
 package principal;
 
 import controller.LoginController;
+import de.javasoft.plaf.synthetica.SyntheticaAluOxideLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaBlackEyeLookAndFeel;
+import de.javasoft.plaf.synthetica.SyntheticaBlackStarLookAndFeel;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UnsupportedLookAndFeelException;
 import model.Usuario;
 import util.EntityManagerUtil;
 import view.LoginFrame;
@@ -17,10 +24,19 @@ import view.LoginFrame;
 public class PokerBASE {
 
     public static void main(String[] args) {
+        try {
+
+            javax.swing.UIManager.setLookAndFeel(new SyntheticaBlackEyeLookAndFeel());
+
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(PokerBASE.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(PokerBASE.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         Usuario usuario = new Usuario();
         LoginFrame loginFrame = new LoginFrame();
-        
-        
+
         LoginController controller = new LoginController(usuario, loginFrame, EntityManagerUtil.emf);
         loginFrame.setVisible(true);
     }
