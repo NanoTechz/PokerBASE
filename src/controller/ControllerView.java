4e53852813793422ffafdf3488ca2f5bb08d9207
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-11 18:30:39 -0200

    Centralizando as "telas" diretamente no superclasse.

---
+++ src/controller/ControllerView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import javax.persistence.EntityManagerFactory;
+import view.BasicaView;
+
+/**
+ *
+ * @author augusto
+ */
+public abstract class ControllerView {
+    private EntityManagerFactory emf;
+
+    public ControllerView(EntityManagerFactory emf, BasicaView view) {
+        this.emf = emf;
+        view.centralizarTela();
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+}
