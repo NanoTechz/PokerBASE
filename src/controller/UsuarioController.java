d03a5482920931d2f7ca47a9d94f749d874f027d
Nanson Augusto <nanson.augusto@hotmail.com>
2013-12-11 18:37:03 -0200

    Organizando os controllers.

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controller;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Usuario;
+import seguranca.Criptografia;
+import view.BasicaView;
+
+/**
+ *
+ * @author augusto
+ */
+public class UsuarioController {
+    
+    private Usuario usuario;
+
+    public UsuarioController(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public boolean antentica(String senha, BasicaView view){
+        try {
+            senha = Criptografia.codificar(senha);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+            view.erroMensagem("Não foi possível criptografar a senha.");
+            return false;
+        }
+        
+        return (senha.equals(usuario.getSenha()));
+    }
+    
+    
+    
+}
